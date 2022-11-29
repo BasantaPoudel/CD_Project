317c38bce845f7d0b3d510d6276c1bb2d5b49097
Nielsvullings <119065861+Nielsvullings@users.noreply.github.com>
2022-11-29 21:42:52 +0000

    Niels lab2 tasks

---
+++ fill_values.py
@@ -0,0 +1,65 @@
+from sklearn.impute import SimpleImputer
+from pandas import concat, DataFrame
+from dscharts import get_variable_types
+from numpy import nan
+import pandas as pd
+import dataframe_image as dfi
+
+data = pd.read_csv('data/classification/lab2_datasets/dataset1/drop_recs_cols_dataset1.csv',
+                   index_col = 'encounter_id')
+
+strategy = 2
+
+if strategy == 1:
+    tmp_nr, tmp_sb, tmp_bool = None, None, None
+    variables = get_variable_types(data)
+    numeric_vars = variables['Numeric']
+    symbolic_vars = variables['Symbolic']
+    binary_vars = variables['Binary']
+    
+    if len(numeric_vars) > 0:
+        imp = SimpleImputer(strategy='constant', fill_value=0, missing_values=nan, copy=True)
+        tmp_nr = DataFrame(imp.fit_transform(data[numeric_vars]), columns=numeric_vars)
+    if len(symbolic_vars) > 0:
+        imp = SimpleImputer(strategy='constant', fill_value='NA', missing_values=nan, copy=True)
+        tmp_sb = DataFrame(imp.fit_transform(data[symbolic_vars]), columns=symbolic_vars)
+    if len(binary_vars) > 0:
+        imp = SimpleImputer(strategy='constant', fill_value=False, missing_values=nan, copy=True)
+        tmp_bool = DataFrame(imp.fit_transform(data[binary_vars]), columns=binary_vars)
+    
+    df = concat([tmp_nr, tmp_sb, tmp_bool], axis=1)
+    df.index = data.index
+    df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    df.to_csv('data/classification/lab2_datasets/dataset1/mv_filled_constant_dataset1.csv', index=True)
+    describe = df.describe(include='all')
+    dfi.export(describe, 'images/missing_values_imputation/dataset1/describe_constant_fill.png',max_cols=(-1))
+
+else:
+    tmp_nr, tmp_sb, tmp_bool = None, None, None
+    variables = get_variable_types(data)
+    numeric_vars = variables['Numeric']
+    symbolic_vars = variables['Symbolic']
+    binary_vars = variables['Binary']
+    
+    tmp_nr, tmp_sb, tmp_bool = None, None, None
+    if len(numeric_vars) > 0:
+        imp = SimpleImputer(strategy='mean', missing_values=nan, copy=True)
+        tmp_nr = DataFrame(imp.fit_transform(data[numeric_vars]), columns=numeric_vars)
+    if len(symbolic_vars) > 0:
+        imp = SimpleImputer(strategy='most_frequent', missing_values=nan, copy=True)
+        tmp_sb = DataFrame(imp.fit_transform(data[symbolic_vars]), columns=symbolic_vars)
+    if len(binary_vars) > 0:
+        imp = SimpleImputer(strategy='most_frequent', missing_values=nan, copy=True)
+        tmp_bool = DataFrame(imp.fit_transform(data[binary_vars]), columns=binary_vars)
+    
+    df = concat([tmp_nr, tmp_sb, tmp_bool], axis=1)
+    df.index = data.index
+    
+    df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    df.to_csv('data/classification/lab2_datasets/dataset1/mv_filled_most_frequent_dataset1.csv', index=True)
+    describe = df.describe(include='all')
+    dfi.export(describe, 'images/missing_values_imputation//dataset1/describe_most_frequent_fill.png', max_cols=(-1))
+    
+    
+    
+    
---
+++ scaling.py
@@ -0,0 +1,47 @@
+from pandas import read_csv
+import pandas as pd
+from pandas.plotting import register_matplotlib_converters
+from dscharts import get_variable_types
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from pandas import DataFrame, concat
+from matplotlib.pyplot import subplots, show
+from matplotlib.pyplot import figure, savefig
+
+register_matplotlib_converters()
+
+data = pd.read_csv('data/classification/lab2_datasets/dataset1/mv_filled_most_frequent_dataset1.csv')
+
+variable_types = get_variable_types(data)
+numeric_vars = variable_types['Numeric']
+symbolic_vars = variable_types['Symbolic']
+boolean_vars = variable_types['Binary']
+
+df_nr = data[numeric_vars]
+df_sb = data[symbolic_vars]
+df_bool = data[boolean_vars]
+
+transf = StandardScaler(with_mean=True, with_std=True, copy=True).fit(df_nr)
+tmp = DataFrame(transf.transform(df_nr), index=data.index, columns= numeric_vars)
+norm_data_zscore = concat([tmp, df_sb,  df_bool], axis=1)
+norm_data_zscore.to_csv('data/classification/lab2_datasets/dataset1/dataset1_scaled_zscore.csv', index=False)
+
+
+transf = MinMaxScaler(feature_range=(0, 1), copy=True).fit(df_nr)
+tmp = DataFrame(transf.transform(df_nr), index=data.index, columns= numeric_vars)
+norm_data_minmax = concat([tmp, df_sb,  df_bool], axis=1)
+norm_data_minmax.to_csv('data/classification/lab2_datasets/dataset1/dataset1_scaled_minmax.csv', index=False)
+print(norm_data_minmax.describe())
+
+
+
+
+fig, axs = subplots(1, 3, figsize=(20,10),squeeze=False)
+axs[0, 0].set_title('Original data')
+data.boxplot(ax=axs[0, 0])
+axs[0, 1].set_title('Z-score normalization')
+norm_data_zscore.boxplot(ax=axs[0, 1])
+axs[0, 2].set_title('MinMax normalization')
+norm_data_minmax.boxplot(ax=axs[0, 2])
+show()
+savefig('images/data_scaling/dataset1/scaling_comparison_dataset1')
+
