34a560675f132676aaf4060da931b34a34455b92
Nielsvullings <119065861+Nielsvullings@users.noreply.github.com>
2022-12-20 20:02:19 +0000

    overfitting study RF DC dataset 1

--- random_forest.py
+++ random_forest.py
@@ -12,7 +12,7 @@ def random_forest(filename, file, dataset, clas):
     target = clas
 
     train: DataFrame = read_csv(f'{filename}_train.csv')
-    # train = train.head(500)
+    train = train.head(5000)
     trnY: ndarray = train.pop(target).values
     trnX: ndarray = train.values
     labels = unique(trnY)
@@ -20,7 +20,7 @@ def random_forest(filename, file, dataset, clas):
     print(labels)
 
     test: DataFrame = read_csv(f'{filename}_test.csv')
-    # test=test.head(300)
+    test=test.head(3000)
     tstY: ndarray = test.pop(target).values
     tstX: ndarray = test.values
 
@@ -32,72 +32,75 @@ def random_forest(filename, file, dataset, clas):
     best_model = None
 
     cols = len(max_depths)
-    figure()
-    fig, axs = subplots(1, cols, figsize=(cols * HEIGHT, HEIGHT), squeeze=False)
-    for k in range(len(max_depths)):
-        d = max_depths[k]
-        values = {}
-        for f in max_features:
-            yvalues = []
-            for n in n_estimators:
-                print(n)
-                rf = RandomForestClassifier(n_estimators=n, max_depth=d, max_features=f)
-                rf.fit(trnX, trnY)
-                prdY = rf.predict(tstX)
-                yvalues.append(accuracy_score(tstY, prdY))
-                if yvalues[-1] > last_best:
-                    best = (d, f, n)
-                    last_best = yvalues[-1]
-                    best_model = rf
+    # figure()
+    # fig, axs = subplots(1, cols, figsize=(cols * HEIGHT, HEIGHT), squeeze=False)
+    # for k in range(len(max_depths)):
+    #     d = max_depths[k]
+    #     values = {}
+    #     for f in max_features:
+    #         yvalues = []
+    #         for n in n_estimators:
+    #             print(n)
+    #             rf = RandomForestClassifier(n_estimators=n, max_depth=d, max_features=f)
+    #             rf.fit(trnX, trnY)
+    #             prdY = rf.predict(tstX)
+    #             yvalues.append(accuracy_score(tstY, prdY))
+    #             if yvalues[-1] > last_best:
+    #                 best = (d, f, n)
+    #                 last_best = yvalues[-1]
+    #                 best_model = rf
+    #
+    #         values[f] = yvalues
+    #     multiple_line_chart(n_estimators, values, ax=axs[0, k], title=f'Random Forests with max_depth={d}',
+    #                         xlabel='nr estimators', ylabel='accuracy', percentage=True)
+    #     savefig('images/random_forest/'+dataset+'/_rf_study.png')
+    # # show()
+    # print('Best results with depth=%d, %1.2f features and %d estimators, with accuracy=%1.2f' % (
+    # best[0], best[1], best[2], last_best))
 
-            values[f] = yvalues
-        multiple_line_chart(n_estimators, values, ax=axs[0, k], title=f'Random Forests with max_depth={d}',
-                            xlabel='nr estimators', ylabel='accuracy', percentage=True)
-        savefig('images/random_forest/'+dataset+'/_rf_study.png')
+    # prd_trn = best_model.predict(trnX)
+    # prd_tst = best_model.predict(tstX)
+    # if dataset == 'dataset1':
+    #     plot_evaluation_results_Maribel(labels, trnY, prd_trn, tstY, prd_tst)
+    #     savefig('images/random_forest/'+dataset+'/_rf_best.png')
+    #     show()
+    # else:
+    #     plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
+    #     savefig('images/random_forest/' + dataset + '/_rf_best.png')
+    #     show()
     # show()
-    print('Best results with depth=%d, %1.2f features and %d estimators, with accuracy=%1.2f' % (
-    best[0], best[1], best[2], last_best))
 
-    prd_trn = best_model.predict(trnX)
-    prd_tst = best_model.predict(tstX)
-    if dataset == 'dataset1':
-        plot_evaluation_results_Maribel(labels, trnY, prd_trn, tstY, prd_tst)
-        savefig('images/random_forest/'+dataset+'/_rf_best.png')
-        show()
-    else:
-        plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
-        savefig('images/random_forest/' + dataset + '/_rf_best.png')
-        show()
-    # show()
+    # variables = train.columns
+    # importances = best_model.feature_importances_
+    # stdevs = std([tree.feature_importances_ for tree in best_model.estimators_], axis=0)
+    # indices = argsort(importances)[::-1]
+    # elems = []
+    # for f in range(len(variables)):
+    #     elems += [variables[indices[f]]]
+    #     print(f'{f + 1}. feature {elems[f]} ({importances[indices[f]]})')
+    #
+    # figure()
+    # horizontal_bar_chart(elems, importances[indices], stdevs[indices], title='Random Forest Features importance',
+    #                      xlabel='importance', ylabel='variables')
+    # savefig('images/random_forest/'+dataset+'/_rf_ranking.png')
 
-    variables = train.columns
-    importances = best_model.feature_importances_
-    stdevs = std([tree.feature_importances_ for tree in best_model.estimators_], axis=0)
-    indices = argsort(importances)[::-1]
-    elems = []
-    for f in range(len(variables)):
-        elems += [variables[indices[f]]]
-        print(f'{f + 1}. feature {elems[f]} ({importances[indices[f]]})')
+    f = 0.7
+    max_depth = 10
+    eval_metric = accuracy_score
+    y_tst_values = []
+    y_trn_values = []
+    for n in n_estimators:
+        rf = RandomForestClassifier(n_estimators=n, max_depth=max_depth, max_features=f)
+        rf.fit(trnX, trnY)
+        prd_tst_Y = rf.predict(tstX)
+        prd_trn_Y = rf.predict(trnX)
+        y_tst_values.append(eval_metric(tstY, prd_tst_Y))
+        y_trn_values.append(eval_metric(trnY, prd_trn_Y))
+    plot_overfitting_study(n_estimators, y_trn_values, y_tst_values, name=f'RF_depth={max_depth}_vars={f}',
+                           xlabel='nr_estimators', ylabel=str(eval_metric))
 
-    figure()
-    horizontal_bar_chart(elems, importances[indices], stdevs[indices], title='Random Forest Features importance',
-                         xlabel='importance', ylabel='variables')
-    savefig('images/random_forest/'+dataset+'/_rf_ranking.png')
-
-    # f = 0.7
-    # max_depth = 10
-    # eval_metric = accuracy_score
-    # y_tst_values = []
-    # y_trn_values = []
-    # for n in n_estimators:
-    #     rf = RandomForestClassifier(n_estimators=n, max_depth=d, max_features=f)
-    #     rf.fit(trnX, trnY)
-    #     prd_tst_Y = rf.predict(tstX)
-    #     prd_trn_Y = rf.predict(trnX)
-    #     y_tst_values.append(eval_metric(tstY, prd_tst_Y))
-    #     y_trn_values.append(eval_metric(trnY, prd_trn_Y))
-    # plot_overfitting_study(n_estimators, y_trn_values, y_tst_values, name=f'RF_depth={max_depth}_vars={f}',
-    #                        xlabel='nr_estimators', ylabel=str(eval_metric))
+    savefig('images/random_forest/'+dataset+'/_overfit_study.png')
+    show()
 
 
 
