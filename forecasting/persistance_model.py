b8c229ef537683c42f1928fb43607c726d922c24
Nielsvullings <119065861+Nielsvullings@users.noreply.github.com>
2023-01-05 22:03:25 +0000

    persistance_plots smoothing & differentiation

--- forecasting/persistance_model.py
+++ forecasting/persistance_model.py
@@ -7,10 +7,10 @@ from matplotlib.pyplot import show, savefig
 
 
 def main(data, dataset, granularity, train, test, prd_trn, prd_tst, index_col, target):
-    plot_evaluation_results(train.values, prd_trn, test.values, prd_tst, 'persistence_eval')
+    plot_evaluation_results(train.values, prd_trn, test.values, prd_tst, 'persistence_eval_'+granularity)
     savefig(f'images/time_series/persistance_model/'+dataset+'/persistence_eval'+granularity+'.png')
 
-    plot_forecasting_series(train, test, prd_trn, prd_tst, 'persistence_plots',
+    plot_forecasting_series(train, test, prd_trn, prd_tst, 'persistence_plots+'+granularity,
                             x_label=index_col,
                             y_label=target)
     savefig(f'images/time_series/persistance_model/'+dataset+'/persistence_plots'+granularity+'.png')
@@ -43,8 +43,9 @@ eval_results = {}
 measure = 'R2'
 
 
-data = read_csv('data/forecasting/aggregation/dataset1/aggregate_daily.csv', index_col='timestamp', sep=',', decimal='.', parse_dates=True, dayfirst=True, infer_datetime_format=True)
+data = read_csv('data/forecasting/aggregation/dataset2/smothing_100_daily.csv', index_col='timestamp', sep=',', decimal='.', parse_dates=True, dayfirst=True, infer_datetime_format=True)
 # data.drop(['Insulin'], axis=1, inplace=True)
+data = data.iloc[100:,:]
 train, test = split_dataframe(data, trn_pct = 0.7)
 
 fr_mod = PersistenceRegressor()
@@ -56,4 +57,4 @@ eval_results['Persistence'] = PREDICTION_MEASURES[measure](test.values, prd_tst)
 print(eval_results)
 
 
-main(data, 'dataset1', 'daily', train, test, prd_trn, prd_tst, 'timestamp', 'Glucose')
+main(data, 'dataset2', 'smoothing_100_daily', train, test, prd_trn, prd_tst, 'timestamp', 'Glucose')
